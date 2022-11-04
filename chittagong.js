550cc06b559a246cdfcb7dd7305d674de6188db2
Abdullah Hamid Sakib <developersakib931@gmail.com>
2022-11-05 02:03:17 +0600

    Initial Commit

---
+++ chittagong.js
@@ -0,0 +1,30 @@
+let PostalCode =[
+    {Thana:'Anowara', Sub_Office:'Anowara', Postal_Code:'4376'},
+    {Thana:'Anowara', Sub_Office:'Battali', Postal_Code:'4378'},
+    {Thana:'Anowara', Sub_Office:'Paroikora', Postal_Code:'4377'},
+    {Thana:'Boalkhali', Sub_Office:'Boalkhali', Postal_Code:'4366'},
+    {Thana:'Boalkhali', Sub_Office:'Charandwip', Postal_Code:'4369'},
+    {Thana:'Boalkhali', Sub_Office:'Iqbal Park', Postal_Code:'4365'},
+    {Thana:'Boalkhali', Sub_Office:'Kadurkhal', Postal_Code:'4368'},
+    {Thana:'Boalkhali', Sub_Office:'Kanungopara', Postal_Code:'4363'},
+    {Thana:'Boalkhali', Sub_Office:'Sakpura', Postal_Code:'4367'},
+    {Thana:'Boalkhali', Sub_Office:'Saroatoli', Postal_Code:'4364'},
+    {Thana:'Fatikchhari', Sub_Office:'Bhandar Sharif', Postal_Code:'4352'},
+    {Thana:'Fatikchhari', Sub_Office:'Fatikchhari', Postal_Code:'4350'},
+    {Thana:'Fatikchhari', Sub_Office:'Harualchhari', Postal_Code:'4354'},
+    {Thana:'Fatikchhari', Sub_Office:'Najirhat', Postal_Code:'4353'},
+    {Thana:'Fatikchhari', Sub_Office:'Nanupur', Postal_Code:'4351'},
+    {Thana:'Fatikchhari', Sub_Office:'Narayanhat', Postal_Code:'4355'},
+    {Thana:'Hathazari', Sub_Office:'Chitt.University', Postal_Code:'4331'},
+    {Thana:'Hathazari', Sub_Office:'Fatahabad', Postal_Code:'4335'},
+    {Thana:'Hathazari', Sub_Office:'Gorduara', Postal_Code:'4332'},
+    {Thana:'Hathazari', Sub_Office:'Hathazari', Postal_Code:'4330'},
+    {Thana:'Hathazari', Sub_Office:'Katirhat', Postal_Code:'4333'},
+    {Thana:'Hathazari', Sub_Office:'Madrasa', Postal_Code:'4339'},
+    {Thana:'Hathazari', Sub_Office:'Mirzapur', Postal_Code:'4334'},
+    {Thana:'Hathazari', Sub_Office:'Nuralibari', Postal_Code:'4337'},
+    {Thana:'Hathazari', Sub_Office:'Yunus Nagar', Postal_Code:'4338'},
+]
+
+export {PostalCode};
+
---
+++ main.js
@@ -0,0 +1,7 @@
+import {PostalCode} from "./chittagong.js";
+
+PostalCode.map(function(item){
+    console.log(`Thana:${item.Thana},`,`Sub-Office:${item.Sub_Office},`,`Postal-Code:${item.Postal_Code}`);
+});
+
+
